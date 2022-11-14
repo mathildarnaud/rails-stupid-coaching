74e8f849cd3824e4007d4c0f60b31438db455d59
Mathilde Arnaud <mathildarnaud0598@gmail.com>
2022-11-14 12:04:17 +0100

    done stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+    # @asks = ["I am going to work", "?"]
+  end
+
+  def answer
+    if params[:answer] == "I am going to work"
+      @answer = "Great!"
+    elsif params[:answer].include?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
